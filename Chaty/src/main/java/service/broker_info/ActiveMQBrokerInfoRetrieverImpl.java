74068591ffb46da319321a32d8c416059a4e1d66
Fotis <f.alatas@student.fontys.nl>
2021-11-05 18:11:25 +0100

    fix small typos

--- Chaty/src/main/java/service/broker_info/ActiveMQBrokerInfoRetrieverImpl.java
+++ Chaty/src/main/java/service/broker_info/ActiveMQBrokerInfoRetrieverImpl.java
@@ -20,7 +20,7 @@ import java.util.Set;
 public class ActiveMQBrokerInfoRetrieverImpl implements ActiveMQBrokerInfoRetriever {
 
     /**
-     * 3. Implement the logic of retrieving the topics you created from ActiveMQ Broker
+     * 4. Implement the logic of retrieving the topics you created from ActiveMQ Broker
      */
 
     /**
@@ -30,18 +30,18 @@ public class ActiveMQBrokerInfoRetrieverImpl implements ActiveMQBrokerInfoRetrie
      */
     @Override
     public List<String> getTopics() {
-        // 3.1 Create an MQService
+        // 4.1 Create an MQService object
         MQService mqService = new ActiveMQService();
         List<String> topicNames = new ArrayList<>();
 
         try {
-            // 3.2 Create an active mq connection using the MQService - note it needs to be of type ActiveMQConnection
+            // 4.2 Create an active mq connection using the MQService - note it needs to be of type ActiveMQConnection
             ActiveMQConnection connection = (ActiveMQConnection) mqService.createConnection();
-            // 3.3 Start the connection
+            // 4.3 Start the connection
             connection.start();
-            // 3.4 Create a DestinationSource object using the previously created connections
+            // 4.4 Create a DestinationSource object using the previously created connections
             DestinationSource destinationSource = connection.getDestinationSource();
-            // 3.5 Get the topics from the destination source
+            // 4.5 Get the topics from the destination source
             Set<ActiveMQTopic> topics = destinationSource.getTopics();
 
             topics.forEach(topic -> {
@@ -54,7 +54,7 @@ public class ActiveMQBrokerInfoRetrieverImpl implements ActiveMQBrokerInfoRetrie
                     e.printStackTrace();
                 }
             });
-            // 3.6 Close the destination source using its stop method and the previously created connection
+            // 4.6 Close the destination source using its stop method and the previously created connection
             destinationSource.stop();
             connection.close();
         } catch (JMSException e) {
